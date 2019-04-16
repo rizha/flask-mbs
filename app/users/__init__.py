f8d109e0b7adb6f3ae4712ed3f5b2765e8b60c18
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-04-16 21:37:17 +0700

    add GET /users

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, jsonify
 from marshmallow import Schema, fields, ValidationError
-from passlib.hash import pbkdf2_sha512
 import peewee
 
 
@@ -11,26 +10,33 @@ users = Blueprint('users', __name__)
 
 class UserSchema(Schema):
     username = fields.Str(required=True, allow_none=False)
-    password = fields.Function(
-        deserialize=lambda obj: pbkdf2_sha512.hash(obj),
-        required=True,
-    )
+    password = fields.Str(required=True, allow_none=False)
 
 
 @users.route('/users', methods=['POST', 'GET'])
 def users_resource():
-
     if request.method == 'POST':
         try:
             payload = request.get_json() or {}
             user, _ = UserSchema(strict=True).load(payload)
+
+            user['password'] = User().password_hash(user['password'])
             User.create(**user)
 
         except ValidationError as e:
             return jsonify(e.messages), 422
 
         except peewee.IntegrityError as e:
-            if 'UNIQUE constraint failed' in repr(e):
+            if 'unique' in repr(e).lower():
                 return jsonify(messages='Username already exists'), 409
 
         return jsonify(user), 201
+
+    elif request.method == 'GET':
+        users = []
+        for user in User.select():
+            users.append(dict(
+                username=user.username,
+                password=user.password
+            ))
+        return jsonify(users)
