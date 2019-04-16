7a689e1dd652bb01bcc84193945c8048df2c4978
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-04-16 22:49:11 +0700

    revise minor

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from app import create_app, init_db, make_celery
 
 @pytest.fixture
 def app():
-    
+
     db_fd, db_path = tempfile.mkstemp()
     app = create_app(config_test=dict(
         TESTING=True,
@@ -16,15 +16,16 @@ def app():
         CELERY_ALWAYS_EAGER=True
     ))
 
-    celery = make_celery(app)
+    make_celery(app)
 
     with app.app_context():
         init_db()
-    
+
     yield app
 
     os.close(db_fd)
     os.unlink(db_path)
+
 
 @pytest.fixture
 def client(app):
