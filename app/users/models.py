cb02ca5e41a52a6d116b2f4664fb4b85723a8b4e
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-04-16 21:37:59 +0700

    add password hash user creation

--- app/users/models.py
+++ app/users/models.py
@@ -2,9 +2,13 @@ from app import db
 
 import peewee
 import uuid
+from passlib.hash import pbkdf2_sha512
 
 
 class User(db.Model):
     id = peewee.UUIDField(primary_key=True, default=uuid.uuid4())
     username = peewee.CharField(unique=True, max_length=50)
     password = peewee.CharField(max_length=200)
+
+    def password_hash(self, password):
+        return pbkdf2_sha512.hash(password)
