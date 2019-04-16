f9b3d32a284a8f797c2bfb8a5202e758680a48e2
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-04-16 18:38:12 +0700

    revise minor

--- app/__init__.py
+++ app/__init__.py
@@ -1,55 +1,59 @@
-
+import importlib
 import logging.config
 
-from flask import Flask, jsonify, current_app
+import click
+from flask import Flask, jsonify
+from flask.cli import with_appcontext
 from werkzeug.contrib.cache import RedisCache
-from peewee import PostgresqlDatabase, SqliteDatabase
+from playhouse.flask_utils import FlaskDB
 
 from app import settings
+
+db = FlaskDB()
 
 
 def create_app(config_test=None):
     logging.config.dictConfig(settings.LOGGING)
     app = Flask(__name__)
     app.config.from_object(settings)
-    
+
     if config_test:
         app.config.update(config_test)
-        db = SqliteDatabase(app.config['DATABASE'])
-    else:
-        db = PostgresqlDatabase(app.config['DATABASE'])
-    
+
+    db.init_app(app)
+
     app.db = db
 
     app.cache = RedisCache(host=settings.REDIS_HOST,
                            port=settings.REDIS_PORT,
                            db=settings.REDIS_DB)
-    
-    from app.users import users
-    app.register_blueprint(users)
-    
+
+    for module, bp in settings.APPS:
+        module = importlib.import_module(module)
+        app.register_blueprint(getattr(module, bp))
+
     @app.shell_context_processor
     def make_shell_context():
         return dict(app=app, db=db)
-
-    @app.before_request
-    def _open_database():
-        app.db.connect(reuse_if_open=True)
-    
-    @app.teardown_request
-    def _close_database(cx):
-        app.db.close()
-
 
     @app.route('/status')
     def status():
         app.logger.info("hi i'm logging GET /status route")
         return jsonify(message='ok')
 
+    @click.command('init_db')
+    @with_appcontext
+    def init_db_command():
+        """Create new tables."""
+        init_db()
+        click.echo('Initialize the tables')
+
+    app.cli.add_command(init_db_command)
+
     return app
 
 
 def init_db():
     from app.users.models import User
-    with current_app.app_context():
-        current_app.db.create_tables([User,])
+    with db.database:
+        db.database.create_tables([User, ])
