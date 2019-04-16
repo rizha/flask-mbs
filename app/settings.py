73e973dca239ad0656dbf1691cf565372361f194
Rizha Musthafa <rizha.musthafa@gmail.com>
2019-04-16 21:37:40 +0700

    disable jsonify sort

--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,6 @@ REDIS_HOST = env('REDIS_HOST', 'localhost')
 REDIS_PORT = env.int('REDIS_PORT', 6379)
 REDIS_DB = env.int('REDIS_DB', 0)
 
-
 DATABASE = env('DATABASE', 'postgresql://postgres:password@localhost:5432/app')
 
 CELERY_RESULT_BACKEND = env(
@@ -18,6 +17,8 @@ CELERY_RESULT_BACKEND = env(
     'redis://localhost:6379/0')
 CELERY_BROKER_URL = env('CELERY_BROKER_URL', 'redis://localhost:6379/0')
 
+
+JSON_SORT_KEYS = False
 
 LOGGING = {
     "version": 1,
